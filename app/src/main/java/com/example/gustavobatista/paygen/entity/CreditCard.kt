6ba14536b9fd1e47f997ea68637f96576e0ed019
gustavo.batista <gustavo.batista@tivit.com>
2018-06-18 09:29:44 -0300

    xfdsfasfas

--- app/src/main/java/com/example/gustavobatista/paygen/entity/CreditCard.kt
+++ app/src/main/java/com/example/gustavobatista/paygen/entity/CreditCard.kt
@@ -4,4 +4,5 @@ package com.example.gustavobatista.paygen.entity
  * Created by Gustavo on 10/23/2017.
  */
 
-class CreditCard
+class CreditCard(var cardNumber: Number, var cardHolder: String, var securityCode: String,
+                 var expiryDate: String)
