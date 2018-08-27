79f99efe0dbde973d38be07b7c1bb96b77a068d0
Gustavo <gustavo.batista@tivit.com>
2018-08-27 00:36:53 -0300

    Lista por localização

--- app/src/main/java/com/example/gustavobatista/paygen/activity/PurchasesActivity.kt
+++ app/src/main/java/com/example/gustavobatista/paygen/activity/PurchasesActivity.kt
@@ -1,32 +1,90 @@
 package com.example.gustavobatista.paygen.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.example.gustavobatista.paygen.R
 import com.example.gustavobatista.paygen.adapter.TransactionAdapter
 import com.example.gustavobatista.paygen.entity.Transaction
 import com.example.gustavobatista.paygen.entity.dto.DateFilter
 import com.example.gustavobatista.paygen.prefs
 import com.example.gustavobatista.paygen.service.TransactionService
-import kotlinx.android.synthetic.main.activity_sales.*
+import kotlinx.android.synthetic.main.activity_purchases.*
 import org.jetbrains.anko.startActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
 class PurchasesActivity : BaseActivity() {
 
+    var startLength = 0
+    var endLength = 0
+    private var locked = false
+    private val sdf: SimpleDateFormat
+        @SuppressLint("SimpleDateFormat")
+        get() = SimpleDateFormat("dd/MM/yyyy")
+
+    private val startWatcher: TextWatcher
+        get() = object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {
+                if (text != null) {
+                    if (text.length > startLength) {
+
+                        if (text.length == 2 || text.length == 5) {
+                            text.append('/')
+                        }
+                    }
+                    startLength = text.length
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        }
+    private val endWatcher: TextWatcher
+        get() = object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {
+                if (text != null) {
+                    if (text.length > endLength) {
+
+                        if (text.length == 2 || text.length == 5) {
+                            text.append('/')
+                        }
+                    }
+                    endLength = text.length
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sales)
+        setContentView(R.layout.activity_purchases)
         setupActionBar()
         recyclerView.layoutManager = LinearLayoutManager(this)
+        etEndDate.addTextChangedListener(endWatcher)
+        etStartDate.addTextChangedListener(startWatcher)
+        btOk.setOnClickListener { filterDate() }
+        lFilter.setOnClickListener { lFilter.visibility = View.GONE }
+
     }
 
     override fun onStart() {
         super.onStart()
-        getSales()
+        getPurchases()
     }
 
 
@@ -38,16 +96,81 @@ class PurchasesActivity : BaseActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.action_one_day -> filterSales(1)
-            R.id.action_seven_days -> filterSales(7)
-            R.id.action_thirty_days -> filterSales(30)
+            R.id.action_one_day -> filterPurchases(1)
+            R.id.action_seven_days -> filterPurchases(7)
+            R.id.action_thirty_days -> filterPurchases(30)
+            R.id.action_custom_date -> filterCustom()
             else -> super.onBackPressed()
         }
         return true
     }
 
-    private fun filterSales(daysAgo: Int) {
+    private fun filterCustom() {
+        locked = true
+        lFilter.visibility = View.VISIBLE
+    }
 
+    private fun filterDate() {
+        var startDate: String = etStartDate.text.toString()
+        var endDate: String = etEndDate.text.toString()
+
+        if (startDate.isEmpty() || endDate.isEmpty()) {
+            showWarning(getString(R.string.warning_empty_fields))
+            return
+        }
+        if (startDate.length != 10 || endDate.length != 10) {
+            showWarning(getString(R.string.warning_invalid_date))
+            return
+        }
+
+        try {
+            val calendar = Calendar.getInstance()
+
+
+            val dateS = sdf.parse(startDate)
+
+            val dateE = sdf.parse(endDate)
+            calendar.time = dateE
+            calendar.add(Calendar.DAY_OF_YEAR, 1)
+            val adjustedDateE = calendar.time
+
+            startDate = sdf.format(dateS)
+            endDate = sdf.format(adjustedDateE)
+
+            if (dateS.after(dateE)) {
+                showWarning(getString(R.string.warning_start_end_date))
+                return
+            }
+
+        } catch (e: Exception) {
+            showWarning(getString(R.string.warning_invalid_date))
+            return
+        }
+
+        locked = false
+        etEndDate.setText("")
+        etStartDate.setText("")
+        lFilter.visibility = View.GONE
+        filterPurchases(startDate, endDate)
+    }
+
+    private fun filterPurchases(startDate: String, endDate: String) {
+        showProgress()
+        TransactionService.filterPurchases(prefs.userId, DateFilter(startDate, endDate)).applySchedulers().subscribe(
+                {
+                    setupAdapter(it)
+                },
+                {
+                    closeProgress()
+                    handleException(it)
+                },
+                {
+                    closeProgress()
+                }
+        )
+    }
+
+    private fun filterPurchases(daysAgo: Int) {
         showProgress()
         TransactionService.filterPurchases(prefs.userId, getFilter(daysAgo)).applySchedulers().subscribe(
                 {
@@ -63,7 +186,7 @@ class PurchasesActivity : BaseActivity() {
         )
     }
 
-    private fun getSales() {
+    private fun getPurchases() {
         showProgress()
         TransactionService.getTransactions(prefs.userId).applySchedulers().subscribe(
                 {
@@ -81,14 +204,18 @@ class PurchasesActivity : BaseActivity() {
 
     private fun setupAdapter(transactions: List<Transaction>) {
         val adapter = TransactionAdapter(transactions) {
-            startActivity<PurchaseDetailsActivity>("transaction" to it)
+            if (!locked) {
+                startActivity<PurchaseDetailsActivity>("transaction" to it)
+            }
         }
         recyclerView.adapter = adapter
     }
 
     private fun getFilter(daysAgo: Int): DateFilter {
-        val sdf = SimpleDateFormat("dd/MM/yyyy")
-        val endDate = sdf.format(Date())
+        val calendar = Calendar.getInstance()
+        calendar.time = Date()
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val endDate = sdf.format(calendar.time)
         val startDate = sdf.format(getDaysAgo(daysAgo))
         return DateFilter(startDate, endDate)
     }
